9b69439b87f870d18a1601261c4814bd630f9252
Nitesh Kumar Pal <nitesh28rocking@gmail.com>
2024-03-20 20:01:43 +0530

    checked!

--- Product-Management-System/src/main/java/com/ecom/service/AdminLoginServiceImpl.java
+++ Product-Management-System/src/main/java/com/ecom/service/AdminLoginServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ecom.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -16,49 +18,58 @@ import com.ecom.repository.CurrentAdminSessionDao;
 import net.bytebuddy.utility.RandomString;
 
 @Service
-public class AdminLoginServiceImpl implements AdminLoginService{
+public class AdminLoginServiceImpl implements AdminLoginService {
+
+	private static final Logger logger = LoggerFactory.getLogger(AdminLoginServiceImpl.class);
 
 	@Autowired
 	private AdminRepository adminRepository;
-	
+
 	@Autowired
 	private CurrentAdminSessionDao currentAdminSessionDao;
 
 	@Override
 	public String logIntoAccount(AdminLoginDto adminLoginDto) throws AdminNotFoundException {
-	
+
+		logger.info("Attempting to log into account for username: {}", adminLoginDto.getUsername());
+
 		Admin admin = adminRepository.findByUsername(adminLoginDto.getUsername());
-		if(admin==null) {
+		if (admin == null) {
+			logger.error("Admin not present with username: {}", adminLoginDto.getUsername());
 			throw new AdminNotFoundException("Admin not present with this usename");
 		}
 		Optional<AdminCurrentSession> isValid = currentAdminSessionDao.findById(admin.getAdminId());
-		
-		if(isValid.isPresent()) {
+
+		if (isValid.isPresent()) {
+			logger.error("Admin with username {} is already logged in", adminLoginDto.getUsername());
 			throw new AdminNotFoundException("admin with this username is already present");
 		}
-		if(admin.getPassword().equals(adminLoginDto.getPassword())) {
+		if (admin.getPassword().equals(adminLoginDto.getPassword())) {
 			String key = RandomString.make(4);
-			AdminCurrentSession adminCurrentSession = new AdminCurrentSession(admin.getAdminId(),key, LocalDateTime.now());
+			AdminCurrentSession adminCurrentSession = new AdminCurrentSession(admin.getAdminId(), key,
+					LocalDateTime.now());
 			currentAdminSessionDao.save(adminCurrentSession);
+			logger.info("Logged in successfully for username: {}", adminLoginDto.getUsername());
 			return "Loggin Successfull with this key :" + key;
-		}else {
+		} else {
+			logger.error("Incorrect password for username: {}", adminLoginDto.getUsername());
 			throw new AdminNotFoundException("password Incorrect...");
 		}
-		
-		
-		
+
 	}
 
 	@Override
 	public String logoutFromAccount(String key) throws AdminNotFoundException {
-		
+		logger.info("Attempting to log out with key: {}", key);
 		AdminCurrentSession adminCurrentSession = currentAdminSessionDao.findByAdminKey(key);
-		if(adminCurrentSession==null) {
+		if (adminCurrentSession == null) {
+			logger.error("Incorrect key provided for logout: {}", key);
 			throw new AdminNotFoundException("Incorrect key..");
-			
+
 		}
 		currentAdminSessionDao.delete(adminCurrentSession);
+		logger.info("Logged out successfully with key: {}", key);
 		return "Logout From Account..";
 	}
-	
+
 }
