f29805e79f488afc5a2ffabf70454850d9f877f7
Nitesh Kumar Pal <nitesh28rocking@gmail.com>
2024-03-20 19:55:43 +0530

    Logger Updated.

--- Product-Management-System/src/main/java/com/ecom/ProductManagementSystemApplication.java
+++ Product-Management-System/src/main/java/com/ecom/ProductManagementSystemApplication.java
@@ -1,14 +1,15 @@
 package com.ecom;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductManagementSystemApplication {
-
+	 private static final Logger logger = LoggerFactory.getLogger(ProductManagementSystemApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementSystemApplication.class, args);
-		System.out.println("Welcome to Product Management System...");
+		  logger.info("-- Welcome to Product Management System --");
 	}
 
 }
